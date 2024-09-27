e113933b78119dc4aeab96451e42fa07afaa26a7
shubham-dev <97054735+shubham-dev-tiwari@users.noreply.github.com>
2024-09-27 18:10:37 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,99 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol:"https",
+        hostname:"images.pexels.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"earthly.dev",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"repository-images.githubusercontent.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"initiate.alphacoders.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"miro.medium.com",
+    
+      },
+      
+      {
+        protocol:"https",
+        hostname:"external-preview.redd.it",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"bs-uploads.toptal.io",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"media.licdn.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"www.kosli.com",
+    
+      },
+      
+      {
+        protocol:"https",
+        hostname:"e0.pxfuel.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"portfolio-shubham-tiwaris-projects-f7999503.vercel.app",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"unblast.com",
+    
+      },
+      
+      {
+        protocol:"https",
+        hostname:"private-user-images.githubusercontent.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"okcredit-blog-images-prod.storage.googleapis.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"vencru.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"www.zarla.com",
+    
+      },
+      {
+        protocol:"https",
+        hostname:"scontent.fixc8-3.fna.fbcdn.net",
+    
+      },
+      
+      
+    ],
+  },
+};
+
+export default nextConfig;
